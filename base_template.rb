3464d2c10396e800169df898e77432d4ed31b612
Andrew Donaldson <andrew@desto.net>
2010-02-25 22:11:57 +0000

    Initial commit

---
+++ base_template.rb
@@ -0,0 +1,26 @@
+run "echo TODO > README"
+
+# gem ''
+# rake "gems:install"
+
+if yes?("Do you want to use RSpec?")
+  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
+  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
+  generate :rspec
+end
+
+git :init
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+git :add => "."
+git :commit => "-m 'Initial commit'"
